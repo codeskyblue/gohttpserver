8bbfec7e10f1049964b990765c233b2884d563f9
chenwenxiao <chenwenxiao@corp.netease.com>
2019-02-21 15:36:10 +0800

    add oauth2

---
+++ oauth2-proxy.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"net/url"
+)
+
+func handleOauth2() {
+
+	http.HandleFunc("/-/user", func(w http.ResponseWriter, r *http.Request) {
+		fullNameMap, _:= url.ParseQuery(r.Header.Get("X-Auth-Request-Fullname"))
+		var fullName string
+		for k := range fullNameMap {
+			fullName = k
+			break
+		}
+		user := &UserInfo{
+			Email:    r.Header.Get("X-Auth-Request-Email"),
+			Name:     fullName,
+			NickName: r.Header.Get("X-Auth-Request-User"),
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		data, _ := json.Marshal(user)
+		w.Write(data)
+	})
+
+}
